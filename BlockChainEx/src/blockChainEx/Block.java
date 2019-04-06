8e20e3c2fd27bd6eca2a082f9e9fce34aa25bf40
Kim DongJin <41800328+kxv1379@users.noreply.github.com>
2019-04-06 15:42:54 +0900

    블록체인 예제

--- BlockChainEx/src/blockChainEx/Block.java
+++ BlockChainEx/src/blockChainEx/Block.java
@@ -1,5 +1,6 @@
 package blockChainEx;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -13,29 +14,50 @@ import java.util.Date;
 public class Block {
 	public String hash;
 	public String previousHash;
-	private String data;
+	private String merkleRoot;
+	public ArrayList<Transaction> transactions = new ArrayList<Transaction>();
 	private long timeStamp;
 	private int nonce;
 	
-	public Block(String data,String previousHash) {
-		this.data=data;
+	public Block(String previousHash) {
 		this.previousHash = previousHash;
 		this.timeStamp = new Date().getTime();
+		
 		this.hash = calculateHash();
 	}
 	
 	public String calculateHash(){
-		String calculatehash= StringUtil.applySha256(previousHash+Long.toString(timeStamp)+Integer.toString(nonce)+data);
+		String calculatehash= StringUtil.applySha256(
+				previousHash+
+				Long.toString(timeStamp)+
+				Integer.toString(nonce)+
+				merkleRoot);
 		return calculatehash;
 	}
 	
 	public void mineBlock(int difficulty) {
+		merkleRoot = StringUtil.getMerkleRoot(transactions);
 		String target = new String(new char[difficulty]).replace('\0', '0');
 		while(!hash.substring(0,difficulty).equals(target)) {
 			nonce++;
 			hash=calculateHash();
 		}
 		System.out.println("Block Mined!! : "+hash);
+	}
+	
+	//Add transactions to this block
+	public boolean addTransaction(Transaction transaction) {
+		//process transaction and check if valid, unless block is genesis block then ignore.
+		if(transaction == null) return false;		
+		if((previousHash != "0")) {
+			if((transaction.processTransaction() != true)) {
+				System.out.println("Transaction failed to process. Discarded.");
+				return false;
+			}
+		}
+		transactions.add(transaction);
+		System.out.println("Transaction Successfully added to Block");
+		return true;
 	}
 
 
--- BlockChainEx/src/blockChainEx/StringUtil.java
+++ BlockChainEx/src/blockChainEx/StringUtil.java
@@ -1,6 +1,12 @@
 package blockChainEx;
 
+import java.security.Key;
 import java.security.MessageDigest;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.util.ArrayList;
+import java.util.Base64;
 /**
  * 
   * @FileName : StringUtil.java
@@ -27,5 +33,59 @@ public class StringUtil {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	//송신자가 자신의 개인키를 이용해 데이터를 암호화하는 메소드
+	public static byte[] applyECDSASig(PrivateKey privateKey, String input) {
+		Signature dsa;
+		byte[] output = new byte[0];
+		try {
+			dsa = Signature.getInstance("ECDSA", "BC");
+			dsa.initSign(privateKey);
+			byte[] strByte = input.getBytes();
+			dsa.update(strByte);
+			byte[] realSig = dsa.sign();
+			output = realSig;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return output;
+	}
+	
+	//암호화된 데이터를 송신자의 공개키로 디코딩하여 데이터의 무결성을 식별하는 메소드
+	public static boolean verifyECDSASig(PublicKey publicKey, String data, byte[] signature) {
+		try {
+			Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+			ecdsaVerify.initVerify(publicKey);
+			ecdsaVerify.update(data.getBytes());
+			return ecdsaVerify.verify(signature);
+		}catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	//타원 곡선 알고리즘으로 생성된 Key를 보기좋게 String으로 반환해 주는 메소드.
+	public static String getStringFromKey(Key key) {
+		return Base64.getEncoder().encodeToString(key.getEncoded());
+	}
+	
+	//Tacks in array of transactions and returns a merkle root.
+	public static String getMerkleRoot(ArrayList<Transaction> transactions) {
+			int count = transactions.size();
+			ArrayList<String> previousTreeLayer = new ArrayList<String>();
+			for(Transaction transaction : transactions) {
+				previousTreeLayer.add(transaction.transactionId);
+			}
+			ArrayList<String> treeLayer = previousTreeLayer;
+			while(count > 1) {
+				treeLayer = new ArrayList<String>();
+				for(int i=1; i < previousTreeLayer.size(); i++) {
+					treeLayer.add(applySha256(previousTreeLayer.get(i-1) + previousTreeLayer.get(i)));
+				}
+				count = treeLayer.size();
+				previousTreeLayer = treeLayer;  
+			}
+			String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
+			return merkleRoot;
+		}
 }
 ;
